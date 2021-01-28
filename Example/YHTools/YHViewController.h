8e20217405519d9ac067d2df2f36cf3da9649da5
小小程序员 <zk1947@163.com>
2021-01-28 12:58:41 +0800

    Initial commit

---
+++ Example/YHTools/YHViewController.h
@@ -0,0 +1,13 @@
+//
+//  YHViewController.h
+//  YHTools
+//
+//  Created by zk1947@163.com on 01/28/2021.
+//  Copyright (c) 2021 zk1947@163.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface YHViewController : UIViewController
+
+@end
---
+++ Example/YHTools/YHViewController.m
@@ -0,0 +1,29 @@
+//
+//  YHViewController.m
+//  YHTools
+//
+//  Created by zk1947@163.com on 01/28/2021.
+//  Copyright (c) 2021 zk1947@163.com. All rights reserved.
+//
+
+#import "YHViewController.h"
+
+@interface YHViewController ()
+
+@end
+
+@implementation YHViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
