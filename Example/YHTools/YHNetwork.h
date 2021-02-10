25cdf6b2b5d566db75040eb5d3d406a92f3f15a0
小小程序员 <zk1947@163.com>
2021-02-10 11:18:58 +0800

    网络框架封装优化

--- Example/YHTools/YHNetwork.h
+++ Example/YHTools/YHNetwork.h
@@ -1,9 +1,8 @@
 //
 //  YHNetwork.h
-//  YHTools_Example
+//  NNProject
 //
-//  Created by 赵凯 on 2021/1/31.
-//  Copyright © 2021 zk1947@163.com. All rights reserved.
+//  Created by 刘昕 on 2021/2/2.
 //
 
 #import <Foundation/Foundation.h>
@@ -13,7 +12,38 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface YHNetwork : NSObject
 
-+(void)postNetInterFace:(NSString *)url param:(id __nullable)param succeed:(void (^)(id data))succeed;
+/// get请求
+/// @param url <#url description#>
+/// @param param <#param description#>
+/// @param succeed <#succeed description#>
++(void)getInterFace:(NSString *)url param:(id __nullable)param succeed:(void (^)(id data))succeed;
+
+/// post请求数据（loading可控、自动处理code、msg）
+/// @param url <#url description#>
+/// @param param <#param description#>
+/// @param loading <#loading description#>
+/// @param succeed <#succeed description#>
++(void)postDataInterFace:(NSString *)url param:(id __nullable)param loading:(BOOL)loading succeed:(void (^)(id data))succeed;
+
+/// post表单提交（必带loading、自动处理code、msg）
+/// @param url <#url description#>
+/// @param param <#param description#>
+/// @param succeed <#succeed description#>
++(void)postFormInterFace:(NSString *)url param:(id __nullable)param succeed:(void (^)(id data))succeed;
+
+/// post请求（完全手动）
+/// @param url <#url description#>
+/// @param param <#param description#>
+/// @param loading <#loading description#>
+/// @param succeed <#succeed description#>
++(void)postNomalInterFace:(NSString *)url param:(id __nullable)param loading:(BOOL)loading succeed:(void (^)(id data))succeed failure:(void (^)(NSError *error))failure;
+
+/// post请求 上传图片
+/// @param url <#url description#>
+/// @param param <#param description#>
+/// @param imgsArr <#imgsArr description#>
+/// @param succeed <#succeed description#>
++(void)postImageInterFace:(NSString *)url param:(id __nullable)param withImgs:(NSMutableArray *)imgsArr succeed:(void (^)(id data))succeed;
 
 @end
 
--- Example/YHTools/YHNetwork.m
+++ Example/YHTools/YHNetwork.m
@@ -1,30 +1,138 @@
 //
 //  YHNetwork.m
-//  YHTools_Example
+//  NNProject
 //
-//  Created by 赵凯 on 2021/1/31.
-//  Copyright © 2021 zk1947@163.com. All rights reserved.
+//  Created by 刘昕 on 2021/2/2.
 //
 
 #import "YHNetwork.h"
 
 @implementation YHNetwork
 
-#pragma mark -post请求二次封装（处理路径拼接、加解密、loading、refresh等）
-+(void)postNetInterFace:(NSString *)url param:(id __nullable)param succeed:(void (^)(id data))succeed{
++(void)getInterFace:(NSString *)url param:(id __nullable)param succeed:(void (^)(id data))succeed{
+    //1.loading显示
+    ShowDefaultLoadingView(@"加载中..");
+    //2.非全路径进行路径拼接
+    if (![url containsString:@"http"]) {
+        url = [NSString stringWithFormat:@"%@%@",KBASEURL,url];
+    }
+    //3.入参处理（加密、添加基本参数如token等）
     
-    //加密解密
-    
-    [YHNetManage postNetData:@"" param:param succeed:^(id  _Nonnull data) {
+    [YHNetManage getNetData:url param:param succeed:^(id  _Nonnull data) {
+        //4.loading收回
+        DismissDefaultHud();
+        //5.出参数据处理（解密、数据格式转换、code判断等）
         
-        
-        
+        //6.传递出参
+        succeed(data);
     } failure:^(NSError * _Nonnull error) {
-        
-        
-        
+        //4.loading收回
+        DismissDefaultHud();
+        //7.错误日志打印
+        NSLog(@"接口%@==报错:%@",url,error);
     }];
+}
+
++(void)postDataInterFace:(NSString *)url param:(id __nullable)param loading:(BOOL)loading succeed:(void (^)(id data))succeed{
+    //1.loading显示
+    if (loading) {
+        ShowDefaultLoadingView(@"加载中..");
+    }
+    //2.非全路径进行路径拼接
+    if (![url containsString:@"http"]) {
+        url = [NSString stringWithFormat:@"%@%@",KBASEURL,url];
+    }
+    //3.入参处理（加密、添加基本参数如token等）
     
+    [YHNetManage postNetData:url param:param succeed:^(id  _Nonnull data) {
+        //4.loading收回
+        DismissDefaultHud();
+        //5.出参数据处理（解密、数据格式转换、code判断等）
+        
+        //6.传递出参
+        succeed(data);
+    } failure:^(NSError * _Nonnull error) {
+        //4.loading收回
+        DismissDefaultHud();
+        //7.错误日志打印
+        NSLog(@"接口%@==报错:%@",url,error);
+    }];
+}
+
++(void)postFormInterFace:(NSString *)url param:(id __nullable)param succeed:(void (^)(id data))succeed{
+    //1.loading显示
+    ShowDefaultLoadingView(@"加载中..");
+    //2.非全路径进行路径拼接
+    if (![url containsString:@"http"]) {
+        url = [NSString stringWithFormat:@"%@%@",KBASEURL,url];
+    }
+    //3.入参处理（加密、添加基本参数如token等）
+    
+    [YHNetManage postNetData:url param:param succeed:^(id  _Nonnull data) {
+        //4.loading收回
+        DismissDefaultHud();
+        //5.出参数据处理（解密、数据格式转换、code判断等）
+        
+        //6.传递出参
+        succeed(data);
+    } failure:^(NSError * _Nonnull error) {
+        //4.loading收回
+        DismissDefaultHud();
+        //7.错误日志打印
+        NSLog(@"接口%@==报错:%@",url,error);
+    }];
+}
+
++(void)postNomalInterFace:(NSString *)url param:(id __nullable)param loading:(BOOL)loading succeed:(void (^)(id data))succeed failure:(void (^)(NSError *error))failure{
+    //1.loading显示
+    if (loading) {
+        ShowDefaultLoadingView(@"加载中..");
+    }
+    //2.非全路径进行路径拼接
+    if (![url containsString:@"http"]) {
+        url = [NSString stringWithFormat:@"%@%@",KBASEURL,url];
+    }
+    //3.入参处理（加密、添加基本参数如token等）
+    
+    [YHNetManage postNetData:url param:param succeed:^(id  _Nonnull data) {
+        //4.loading收回
+        DismissDefaultHud();
+        //5.出参数据处理（解密、数据格式转换）
+        
+        //6.传递出参
+        NSDictionary *resultDic = [YHTool dataToDic:data];
+        succeed(resultDic);
+    } failure:^(NSError * _Nonnull error) {
+        //4.loading收回
+        DismissDefaultHud();
+        //7.错误回调
+        failure(error);
+    }];
+}
+
++(void)postImageInterFace:(NSString *)url param:(id __nullable)param withImgs:(NSMutableArray *)imgsArr succeed:(void (^)(id data))succeed{
+    //1.loading显示
+    ShowDefaultLoadingView(@"加载中..");
+    //2.非全路径进行路径拼接
+    if (![url containsString:@"http"]) {
+        url = [NSString stringWithFormat:@"%@%@",KBASEURL,url];
+    }
+    //3.入参处理（加密、添加基本参数如token等）
+    
+    [YHNetManage postImgNetData:url param:param withImgs:imgsArr succeed:^(id  _Nonnull data) {
+        //4.loading收回
+        DismissDefaultHud();
+        //5.出参数据处理（解密、数据格式转换、code判断等）
+        
+        //6.传递出参
+        NSDictionary *resultDic = [YHTool dataToDic:data];
+        succeed(resultDic);
+    } failure:^(NSError * _Nonnull error) {
+        //4.loading收回
+        DismissDefaultHud();
+        //7.错误日志打印
+        NSLog(@"接口%@==报错:%@",url,error);
+    }];
 }
 
 @end
